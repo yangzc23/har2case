7f1bf6821f432dbdcb31b27418401ae5a7b829d5
yangzc <testin123>
2020-02-26 23:26:13 +0800

    har2case二次开发

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # har2case
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20200226102358197.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzM3MjUxODk3,size_16,color_FFFFFF,t_70)
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20200226103309788.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzM3MjUxODk3,size_16,color_FFFFFF,t_70)
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20200226103438700.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzM3MjUxODk3,size_16,color_FFFFFF,t_70)
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20200226103835403.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzM3MjUxODk3,size_16,color_FFFFFF,t_70)
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20200226104203647.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzM3MjUxODk3,size_16,color_FFFFFF,t_70)
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/20200226104242419.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3FxXzM3MjUxODk3,size_16,color_FFFFFF,t_70)
+
+**微信扫一扫关注公众号**
+
+![image.png](https://imgconvert.csdnimg.cn/aHR0cHM6Ly91cGxvYWQtaW1hZ2VzLmppYW5zaHUuaW8vdXBsb2FkX2ltYWdlcy8xMDAyNzkwMC0yMzM2ODlhNjQ1YTM2YTEyLnBuZw?x-oss-process=image/format,png#pic_center)
+
+**点击链接加入群聊**
+
+[https://jq.qq.com/?_wv=1027&k=5eVEhfN](https://jq.qq.com/?_wv=1027&k=5eVEhfN)
+
+软件测试学习交流QQ群号：**511619105**
+
+**软件测试学习资料**
+
+[《自动化测试教程》](https://item.taobao.com/item.htm?id=601801208861)
---
+++ cli.py
@@ -0,0 +1,83 @@
+""" Convert HAR (HTTP Archive) to YAML/JSON testcase for HttpRunner.
+
+Usage:
+    # convert to JSON format testcase
+    >>> har2case demo.har
+
+    # convert to YAML format testcase
+    >>> har2case demo.har -2y
+
+"""
+import argparse
+import logging
+import sys
+from distutils.version import StrictVersion
+
+from __about__ import __description__, __version__
+from core import HarParser
+
+try:
+    from httprunner.__about__ import __version__ as HRUN_VERSION
+except ImportError:
+    HRUN_VERSION = None
+
+
+def main():
+    """ HAR converter: parse command line options and run commands.
+    """
+    parser = argparse.ArgumentParser(description=__description__)
+    parser.add_argument(
+        '-V', '--version', dest='version', action='store_true',
+        help="show version")
+    parser.add_argument(
+        '--log-level', default='INFO',
+        help="Specify logging level, default is INFO.")
+    parser.add_argument('har_source_file', nargs='?',
+        help="Specify HAR source file")
+    parser.add_argument(
+        '-2y', '--to-yml', '--to-yaml',
+        dest='to_yaml', action='store_true',
+        help="Convert to YAML format, if not specified, convert to JSON format by default.")
+    parser.add_argument(
+        '-fmt', '--format',
+        dest='fmt_version',
+        help="Specify YAML/JSON testcase format version, v2 corresponds to HttpRunner 2.2.0+.")
+    parser.add_argument(
+        '--filter', help="Specify filter keyword, only url include filter string will be converted.")
+    parser.add_argument(
+        '--exclude',
+        help="Specify exclude keyword, url that includes exclude string will be ignored, multiple keywords can be joined with '|'")
+
+    args = parser.parse_args()
+
+    if args.version:
+        print("{}".format(__version__))
+        exit(0)
+
+    log_level = getattr(logging, args.log_level.upper())
+    logging.basicConfig(level=log_level)
+
+    har_source_file = args.har_source_file
+    if not har_source_file or not har_source_file.endswith(".har"):
+        logging.error("HAR file not specified.")
+        sys.exit(1)
+
+    output_file_type = "YML" if args.to_yaml else "JSON"
+
+    # set default format version by HttpRunner version
+    if HRUN_VERSION and StrictVersion(HRUN_VERSION) < StrictVersion("2.2.0"):
+        default_fmt_version = "v1"
+    else:
+        default_fmt_version = "v2"
+
+    fmt_version = args.fmt_version or default_fmt_version
+
+    HarParser(
+        har_source_file, args.filter, args.exclude
+    ).gen_testcase(output_file_type, fmt_version)
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
